a64ad8b357ffc8930495285322297f77bc744b37
MoraruMatei <moraru.matei24@gmail.com>
2024-02-18 04:31:29 -0500

    Finish game loop

--- main.cpp
+++ main.cpp
@@ -40,16 +40,6 @@ string get_card(string cards[])
 	return card;
 }
 
-int loop()
-{
-	int bet;
-	cout << "How much would you like to bet?" << endl;
-	cin >> bet;
-	if(bet == -1)
-		return 0;
-
-}
-
 void generate_cards(string cards[])
 {
 	string card_names[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
@@ -60,7 +50,6 @@ void generate_cards(string cards[])
 	{
 		for(int j = 0; j < 4; j++)
 		{
-			//cout << i << ' ' << j << ' ' << idx << endl;
 			cards[idx] = card_names[i] + card_colors[j];
 			idx++;
 		}
@@ -87,6 +76,91 @@ void print_cards(string cards[], int len)
 	cout << endl;
 }
 
+
+int loop(string cards[], string player[], int p_len, string dealer[], int d_len, int& balance)
+{
+    cout << "\n\n\n";
+	int bet, move;
+    int p_sum = calculate_cards(player, p_len);
+    int d_sum = calculate_cards(dealer, d_len);
+          
+    cout << "Balance: " << *balance << endl;
+    cout << "How much would you like to bet?" << endl;
+    cin >> bet;
+    while(bet > balance)
+    {
+        cout << "Your current balance is only: " << balance << endl;
+        cin >> bet;
+    }
+    *balance -= bet;
+    cout << "Dealers's cards:" << endl;
+    print_cards(dealer, d_len);
+    cout << "Dealer sum: " << d_sum;
+    cout << endl;
+   
+   cout << "Here are your cards:" << endl;
+    print_cards(player, p_len);
+    cout << "Cards sum: " << p_sum << endl;
+    cout << endl;
+
+    while(move != 0)
+    {
+        cout << "Stand(0) or Hit(1)?" << endl;
+        cin >> move;
+        if(move == 1)
+        {
+            player[p_len] = get_card(cards);
+            p_len++;
+            p_sum = calculate_cards(player, p_len);
+        }
+        cout << "Cards: ";
+        print_cards(player, p_len);
+        cout << "Sum: ";
+        cout << p_sum;
+        cout << endl;
+        if(p_sum > 21)
+        {
+            cout << "You lost!" << endl;
+            return 0;
+        }
+    }
+
+    while(d_sum <= 16)
+    {
+        dealer[d_len] = get_card(cards);
+        d_len++;
+        d_sum = calculate_cards(dealer, d_len);
+        cout << "Dealer cards: "; 
+        print_cards(dealer, d_len);
+        cout << "Dealer sum: " << d_sum << endl;
+        if(d_sum > 21)
+        {
+            cout << "Dealer's busted!" << endl;
+            balance += bet;
+            return 0;
+        }   
+    }
+
+    if(p_sum > d_sum)
+    {
+        cout << "You win!" << endl;
+        cout << "Prize: " << bet * 2 << endl;
+        balance += bet * 2;
+    }
+    else if(d_sum > p_sum)
+    {
+        cout << "You lost! :(" << endl;
+    }
+    else if(d_sum == p_sum)
+    {
+        cout << "Draw!" << endl;
+        if(p_sum == 21)
+            balance += bet;
+        balance += bet;
+    }
+    return 0;
+}
+
 int main()
 {
 	srand(time(0));
@@ -94,7 +168,8 @@ int main()
 	string cards[52];
 	string player[10];
 	string dealer[10];
-	
+    int p_len = 2, d_len = 2;
+	int balance = 100;
 	cout << "Hello there" << endl;
 
 	generate_cards(cards);
@@ -102,15 +177,23 @@ int main()
 
 	player[0] = get_card(cards);
 	player[1] = get_card(cards);
-
-	print_cards(player, 2);
+    dealer[0] = get_card(cards);
+    dealer[1] = get_card(cards);
 
 	int event = -1;
 	while(playing)
 	{
-		event = loop();
-		if(event == 0)
+		event = loop(cards, player, p_len, dealer, d_len, &balance);
+		cout << balance;
+        if(event == -1)
 			playing = false;
-	}
+	    
+	    player[0] = get_card(cards);
+	    player[1] = get_card(cards);
+        dealer[0] = get_card(cards);
+        dealer[1] = get_card(cards);
+        p_len = 2;
+        d_len = 2;
+    }
 }
 
